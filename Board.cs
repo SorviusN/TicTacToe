9ccbb173bc67ad35b5305a242a1948d02f07de0f
SorviusN <Opt1206@gmail.com>
2021-07-16 01:36:25 -0700

    Working Board - Formatted

---
+++ Board.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab04_TicTacToe.Classes
+{
+	class Board
+	{
+		/// <summary>
+		/// Tic Tac Toe Gameboard states
+		/// </summary>
+		public string[,] GameBoard = new string[,] // multi-dimensional array
+		{
+			{"1", "2", "3"},
+			{"4", "5", "6"},
+			{"7", "8", "9"},
+		};
+
+		// Displays the board for the player to see in the console.
+		public void DisplayBoard() 
+		{
+			Console.WriteLine(GameBoard);
+		}
+	}
+}
---
+++ TicTacToe/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Lab04_TicTacToe.Classes;
+
+namespace Lab04_TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StartGame();
+        }
+
+        static void StartGame()
+        {
+            Player PlayerOne = new Player();
+            Player PlayerTwo = new Player();
+            Game Game = new(PlayerOne, PlayerTwo);
+            Player Winner = Game.Play();
+
+            if (Winner != PlayerOne || Winner != PlayerTwo)
+            {
+                Console.WriteLine("There was a draw!");
+            }
+
+            else
+            {
+                Console.WriteLine($"{Winner} Wins!");
+            }
+        }
+    }
+}
